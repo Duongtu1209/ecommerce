5cafaf1b1526d4c7a20eea30563a02d482a3cde7
dT <duongtu1209@gmail.com>
2024-06-06 20:11:39 +0000

    handle call data backend

--- src/App.js
+++ src/App.js
@@ -1,27 +1,39 @@
-import React, { Fragment } from 'react'
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
-import { routes } from './routes'
-import Default from './components/Default/Default'
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { routes } from "./routes";
+import Default from "./components/Default/Default";
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
+
 function App() {
+  const fetchApi = async () => {
+    const res = await axios.get(`${process.env.REACT_APP_URL_BACKEND}/product`);
+    return res.data;
+  };
+  const query = useQuery({ queryKey: ["todos"], queryFn: fetchApi });
+
   return (
     <div>
       <Router>
         <Routes>
           {routes.map((route) => {
-            const Page = route.page
-            const Layout = route.isShowheader ? Default: Fragment
+            const Page = route.page;
+            const Layout = route.isShowheader ? Default : Fragment;
             return (
-              <Route path={route.path} element={
-                <Layout>
-                   <Page/>
-                </Layout>
-            }/>
-            )
+              <Route
+                path={route.path}
+                element={
+                  <Layout>
+                    <Page />
+                  </Layout>
+                }
+              />
+            );
           })}
         </Routes>
       </Router>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,29 +1,35 @@
-import { Image } from 'antd';
-import React from 'react'
-import { WrapperSliderStyle } from './style';
+import { Image } from "antd";
+import React from "react";
+import { WrapperSliderStyle } from "./style";
 
 const SliderComponent = ({ arrImages }) => {
-    const settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 1000
-    };
-    return (
-        <WrapperSliderStyle {...settings}>
-            {arrImages.map((image) => {
-                return (
-                    <div>
-                        <Image key={image} src={image} alt="slider" preview={false} width="100%" height="550px" />
-                    </div>
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+  };
+  return (
+    <WrapperSliderStyle {...settings}>
+      {arrImages.map((image) => {
+        return (
+          <div>
+            <Image
+              key={image}
+              src={image}
+              alt="slider"
+              preview={false}
+              width="100%"
+              height="550px"
+            />
+          </div>
+        );
+      })}
+    </WrapperSliderStyle>
+  );
+};
 
-                )
-            })}
-        </WrapperSliderStyle>
-    )
-}
-
-export default SliderComponent
+export default SliderComponent;
